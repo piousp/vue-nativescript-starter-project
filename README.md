2c7e38d0ebff3add46f55b3510d1bd828d752df1
Rodrigo París <rodrigo.paris@ciriscr.com>
2018-03-02 17:23:45 -0600

    Refactor: Cambiar el template de NativeScript porque tenia una fuga en JDK o Node que solo dejaba compilar una vez

--- README.md
+++ README.md
@@ -1,51 +1,20 @@
-# vue-base
-- Contruído con Node 8.9.4
-- Contruído con NPM 5.6.0
+# movil2
+
+> NativeScript-Vue application
+
+## Usage
 
 ``` bash
+# Install dependencies
 npm install
+
+# Build for production
+npm run build:android
+npm run build:ios
+
+# Build, watch for changes and run the application
+npm run watch:android
+npm run watch:ios
 ```
 
-## Scripts de ejecución
-### Bump
-Hay que hacer el bump
-
-### Lint
-Hay que meterle linter
-
-### Tests (Unidad)
-Hay que meterle tests
-
-### Correr para desarrollo y debug
-Se ejecuta webpack
-``` bash
-npm run webpack-<plataforma>
-Ej.
-npm run webpack-android
-```
-Y en otra terminal hace:
-``` bash
-cd tns
-```
-Y corre debug o run
-#### Debug
-``` bash
-tns debug <plataforma>
-Ej.
-tns debug android
-```
-
-#### Run
-``` bash
-tns run <plataforma>
-Ej.
-tns run android
-```
-
-### Info Extra:
- - Si se le jode el comando de tns porque jdk, borre la carpeta .gradle dentro de tns/platforms/android
- - Por alguna razon no reconoce estilos
-
-###
-
-For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+For detailed instructions, see https://github.com/nativescript-vue/vue-cli-template
--- webpack.config.js
+++ webpack.config.js
@@ -1,219 +1,109 @@
-const { resolve, join } = require("path");
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const NativeScriptVueTarget = require('nativescript-vue-target');
 
-const webpack = require("webpack");
-const nsWebpack = require("nativescript-dev-webpack");
-const nativescriptTarget = require("nativescript-dev-webpack/nativescript-target");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-var MergeFilesPlugin = require('merge-files-webpack-plugin');
+// Generate platform-specific webpack configuration
+module.exports = platform => {
 
-const extractMainSheet = new ExtractTextPlugin('app-0.css');
-const extractCSS = new ExtractTextPlugin('app-1.css');
-
-const mainSheet = `app.css`;
-
-module.exports = env => {
-  const platform = getPlatform(env);
-
-  // tns/app -or- default destination inside platforms/<platform>/...
-  const path = env.tns ? resolve('tns/app') : resolve(nsWebpack.getAppPath(platform));
-
-  const entry = {
-    // Discover entry module from package.json
-    bundle: `./${nsWebpack.getEntryModule()}`,
-
-    // Vendor entry with third-party libraries
-    vendor: `./vendor`,
-
-    // Entry for stylesheet with global application styles
-    [mainSheet]: `./${mainSheet}`,
-  };
-
-  const rules = getRules();
-  const plugins = getPlugins(platform, env);
-  const extensions = getExtensions(platform);
+  // CSS / SCSS style extraction loaders
+  const cssLoader = ExtractTextPlugin.extract({
+    use: [
+      {
+        loader: 'css-loader',
+        options: {url: false},
+      },
+    ],
+  });
+  const scssLoader = ExtractTextPlugin.extract({
+    use: [
+      {
+        loader: 'css-loader',
+        options: {url: false},
+      },
+      'sass-loader',
+    ],
+  });
 
   return {
-    context: resolve("./app"),
-    target: nativescriptTarget,
-    entry,
+    target: NativeScriptVueTarget,
+    entry: path.resolve(__dirname, './src/main.js'),
     output: {
-      pathinfo: true,
-      path,
-      libraryTarget: "commonjs2",
-      filename: "[name].js",
+      path: path.resolve(__dirname, './dist/app'),
+      filename: `app.${platform}.js`,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.css$/,
+          use: cssLoader,
+        },
+        {
+          test: /\.scss$/,
+          use: scssLoader,
+        },
+        {
+          test: /\.vue$/,
+          loader: 'ns-vue-loader',
+          options: {
+            loaders: {
+              css: cssLoader,
+              scss: scssLoader,
+            },
+          },
+        },
+      ],
     },
     resolve: {
-      extensions,
       alias: {
-            '~': join(__dirname, 'tns', 'app')
+        '~': path.resolve(__dirname, './src/assets'),
       },
-
-      // Resolve {N} system modules from tns-core-modules
       modules: [
-        "node_modules/tns-core-modules",
-        "node_modules",
-      ]
+        'node_modules/tns-core-modules',
+        'node_modules',
+      ],
+      extensions: [
+        `.${platform}.js`,
+        '.js',
+        `.${platform}.vue`,
+        '.vue',
+      ],
     },
+    externals (context, request, callback) {
+      if (context.indexOf('tns-core-modules') !== -1
+        || context.indexOf('nativescript-vue') !== -1
+        || /^(tns-core-modules)/i.test(request)) {
+        return callback(null, 'commonjs ' + request);
+      }
+      callback();
+    },
+    plugins: [
+      new ExtractTextPlugin({filename: `app.${platform}.css`}),
+      new OptimizeCssAssetsPlugin({
+        cssProcessor: require('cssnano'),
+        cssProcessorOptions: {discardComments: {removeAll: true}},
+        canPrint: false,
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {warnings: false},
+        output: {comments: false},
+      }),
+      new CopyWebpackPlugin([
+        {from: 'assets', context: 'src'},
+      ]),
+    ],
     node: {
-      // Disable node shims that conflict with NativeScript
-      "http": false,
-      "timers": false,
-      "setImmediate": false,
-      "fs": "empty",
+      'http': false,
+      'timers': false,
+      'setImmediate': false,
+      'fs': 'empty',
     },
-    module: { rules },
-    plugins,
   };
 };
-
-function getPlatform(env) {
-  return env.android ? "android" :
-  env.ios ? "ios" :
-  () => { throw new Error("You need to provide a target platform!") };
-}
-
-function getRules() {
-  return [
-    {
-      test: /\.js$/,
-      loader: 'babel-loader',
-      include: [resolve('app')]
-    },
-    {
-      test: /\.html$|\.xml$/,
-      use: [
-        "raw-loader",
-      ]
-    },
-    // Root stylesheet gets extracted with bundled dependencies
-    {
-      test: new RegExp(mainSheet),
-      loader: extractMainSheet.extract([
-        {
-          loader: "resolve-url-loader",
-          options: { silent: true },
-        },
-        {
-          loader: "nativescript-css-loader",
-          options: { minimize: false }
-        },
-        "nativescript-dev-webpack/platform-css-loader",
-      ]),
-    },
-    // Other CSS files get bundled using the raw loader
-    {
-      test: /\.css$/,
-      exclude: new RegExp(mainSheet),
-      loader: extractCSS.extract({
-        fallback: 'style-loader',
-        use: {
-          loader: 'css-loader',
-          options: { url: false }
-        }
-      })
-
-    },
-    // SASS support
-    {
-      test: /\.s[a|c]ss$/,
-      loader: extractCSS.extract({
-        use: [
-          {
-            loader: 'css-loader',
-            options: { url: false }
-          },
-          'sass-loader'
-        ],
-        fallback: 'vue-style-loader'
-      })
-
-    },
-    // .vue single file component support
-    {
-      test: /\.vue$/,
-      loader: 'ns-vue-loader',
-      options: {
-        loaders: {
-          css: extractCSS.extract("css-loader"),
-          scss: extractCSS.extract({
-            use: [
-              {
-                loader: 'css-loader',
-                options: { url: false }
-              },
-              'sass-loader'
-            ],
-            fallback: 'vue-style-loader'
-          })
-        }
-      }
-    }
-  ];
-}
-
-function getPlugins(platform, env) {
-  let plugins = [
-    extractMainSheet,
-    extractCSS,
-
-    new MergeFilesPlugin({
-      filename: 'app.css',
-      test: /app-[0-1]\.css/,
-      deleteSourceFiles: true
-    }),
-
-    // Vendor libs go to the vendor.js chunk
-    new webpack.optimize.CommonsChunkPlugin({
-      name: ["vendor"],
-    }),
-
-    // Define useful constants like TNS_WEBPACK
-    new webpack.DefinePlugin({
-      "global.TNS_WEBPACK": "true",
-    }),
-
-    // Copy assets to out dir. Add your own globs as needed.
-    new CopyWebpackPlugin([
-      //{ from: mainSheet },
-      { from: "css/**" },
-      { from: "fonts/**" },
-      { from: "**/*.jpg" },
-      { from: "**/*.png" },
-      { from: "**/*.xml" },
-    ], { ignore: ["App_Resources/**"] }),
-
-    // Generate a bundle starter script and activate it in package.json
-    new nsWebpack.GenerateBundleStarterPlugin([
-      "./vendor",
-      "./bundle",
-    ]),
-  ];
-
-  if (env.uglify) {
-    plugins.push(new webpack.LoaderOptionsPlugin({ minimize: true }));
-
-    // Work around an Android issue by setting compress = false
-    const compress = platform !== "android";
-    plugins.push(new webpack.optimize.UglifyJsPlugin({
-      mangle: { except: nsWebpack.uglifyMangleExcludes },
-      compress,
-    }));
-  }
-
-  return plugins;
-}
-
-// Resolve platform-specific modules like module.android.js
-function getExtensions(platform) {
-  return Object.freeze([
-    `.${platform}.js`,
-    ".js",
-    `.${platform}.css`,
-    ".css",
-    `.${platform}.scss`,
-    ".scss",
-    `.${platform}.vue`,
-    ".vue",
-  ]);
-}
