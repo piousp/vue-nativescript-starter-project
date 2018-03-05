e6948ffe03b63abc32d8def018e3fc470ec5b81c
Rodrigo París <rodrigo.paris@ciriscr.com>
2018-03-05 09:14:31 -0600

    Fix: cambiar el template para que tenga el nombre de la aplicación

---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <Page>
+    <ActionBar title="Home"/>
+    <StackLayout class="home">
+      <Button @tap="$router.push('/counter')" text="Counter"/>
+      <Button @tap="$router.push('/hello')" text="Hello"/>
+    </StackLayout>
+  </Page>
+</template>
